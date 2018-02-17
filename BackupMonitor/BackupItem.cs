42491b5e962ae0c568a43f39e113bea4203e9628
pilsnerd <bsjoseph@gmail.com>
2018-02-17 11:20:40 -0600

    add the rest of the files

---
+++ BackupMonitor/BackupItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupMonitor
+{
+    public class BackupItem : INotifyPropertyChanged
+    {
+        public int BackupItemId { get; set; }
+        public string Name { get; set; }
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set { _status = value; OnPropertyChanged(); }
+        }
+
+        private string _note;        
+        public string Note
+        {
+            get { return _note; }
+            set { _note = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string memberName = "")
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(memberName));
+            }
+        }
+    }
+}
---
+++ BackupMonitor/Repository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupMonitor
+{
+    class Repository
+    {
+        private readonly string _connString = ConfigurationManager.ConnectionStrings["NASDB"].ToString();
+
+        public BindingList<BackupItem> ReadBackupItems()
+        {
+            BindingList<BackupItem> items = new BindingList<BackupItem>();
+
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM BackupItems (NOLOCK) WHERE IsDeleted = 0 ORDER BY SourcePath";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        BackupItem item = new BackupItem();
+                        item.BackupItemId = int.Parse(reader["BackupItemID"].ToString());
+                        item.Name = reader["Name"].ToString();
+                        item.SourcePath = reader["SourcePath"].ToString();
+                        item.TargetPath = reader["TargetPath"].ToString();
+                        item.Status = "LOADING...";
+                        items.Add(item);
+                    }
+                    reader.Close();
+                }
+            }
+
+            return items;
+        }
+
+        public void CreateBackupItem(BackupItem newItem)
+        {
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                conn.Open();
+                string query = "INSERT BackupItems (Name, SourcePath, TargetPath) ";
+                query += " VALUES ('";
+                query += newItem.Name;
+                query += "', '";
+                query += newItem.SourcePath;
+                query += "', '";
+                query += newItem.TargetPath;
+                query += "')";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+    }
+}
